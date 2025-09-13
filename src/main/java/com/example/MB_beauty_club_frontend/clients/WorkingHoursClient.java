9c633f8b450432005b4af1acd590f8d0ae020814
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-09-13 18:36:14 +0300

    Working hours done

---
+++ src/main/java/com/example/MB_beauty_club_frontend/clients/WorkingHoursClient.java
@@ -0,0 +1,27 @@
+package com.example.MB_beauty_club_frontend.clients;
+
+import com.example.MB_beauty_club_frontend.dtos.WorkingHoursDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
+
+@FeignClient(name = "MB-working-hours", url = "${backend.base-url}/working-hours")
+public interface WorkingHoursClient {
+
+    @GetMapping
+    List<WorkingHoursDTO> getWorkingHours(@RequestHeader("Authorization") String auth);
+
+    @PutMapping
+    void setWorkingHours(@RequestBody List<WorkingHoursDTO> newHours, @RequestHeader("Authorization") String auth);
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id, @RequestHeader("Authorization") String auth);
+
+}
---
+++ src/main/java/com/example/MB_beauty_club_frontend/controllers/WorkingHoursController.java
@@ -0,0 +1,123 @@
+package com.example.MB_beauty_club_frontend.controllers;
+
+import com.example.MB_beauty_club_frontend.clients.WorkingHoursClient;
+import com.example.MB_beauty_club_frontend.dtos.WorkingHoursDTO;
+import com.example.MB_beauty_club_frontend.dtos.WorkingHoursWrapperDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/working-hours")
+public class WorkingHoursController {
+
+    private final WorkingHoursClient workingHoursClient;
+
+    @GetMapping
+    public String showWorkingHours(Model model, HttpServletRequest request) {
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        String userRole = (String) request.getSession().getAttribute("sessionRole");
+
+        if (userRole == null || (!userRole.equals("WORKER") && !userRole.equals("ADMIN"))) {
+            return "forward:/error";
+        }
+
+        List<WorkingHoursDTO> existingHours = workingHoursClient.getWorkingHours(token);
+        Map<DayOfWeek, WorkingHoursDTO> existingHoursMap = existingHours.stream()
+                .collect(Collectors.toMap(WorkingHoursDTO::getDayOfWeek, dto -> dto));
+
+        List<WorkingHoursDTO> allDaysList = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            if (existingHoursMap.containsKey(day)) {
+                allDaysList.add(existingHoursMap.get(day));
+            } else {
+                WorkingHoursDTO emptyHour = new WorkingHoursDTO();
+                emptyHour.setDayOfWeek(day);
+                allDaysList.add(emptyHour);
+            }
+        }
+
+        model.addAttribute("workingHours", allDaysList);
+        return "WorkingHours/show";
+    }
+
+    @GetMapping("/edit")
+    public String getWorkingHours(Model model, HttpServletRequest request) {
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        String userRole = (String) request.getSession().getAttribute("sessionRole");
+
+        if (userRole == null || !userRole.equals("WORKER")) {
+            return "forward:/error";
+        }
+
+        List<WorkingHoursDTO> existingHours = workingHoursClient.getWorkingHours(token);
+
+        Map<DayOfWeek, WorkingHoursDTO> existingHoursMap = existingHours.stream()
+                .collect(Collectors.toMap(WorkingHoursDTO::getDayOfWeek, dto -> dto));
+
+        List<WorkingHoursDTO> allDaysList = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            if (existingHoursMap.containsKey(day)) {
+                allDaysList.add(existingHoursMap.get(day));
+            } else {
+                WorkingHoursDTO emptyHour = new WorkingHoursDTO();
+                emptyHour.setDayOfWeek(day);
+                allDaysList.add(emptyHour);
+            }
+        }
+
+        WorkingHoursWrapperDTO wrapperDTO = new WorkingHoursWrapperDTO();
+        wrapperDTO.setWorkingHours(allDaysList);
+        model.addAttribute("workingHoursWrapper", wrapperDTO);
+
+        return "WorkingHours/edit";
+    }
+
+    @PostMapping("/edit")
+    public String setWorkingHours(@ModelAttribute WorkingHoursWrapperDTO workingHoursWrapper, HttpServletRequest request) {
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        String userRole = (String) request.getSession().getAttribute("sessionRole");
+
+        if (userRole == null || !userRole.equals("WORKER")) {
+            return "forward:/error";
+        }
+
+        List<WorkingHoursDTO> filteredHours = workingHoursWrapper.getWorkingHours().stream()
+                .filter(wh -> wh.getStartTime() != null &&
+                        wh.getEndTime() != null)
+                .collect(Collectors.toList());
+
+        workingHoursClient.setWorkingHours(filteredHours, token);
+        return "redirect:/working-hours/edit?success=true";
+    }
+
+    @PostMapping("/{id}")
+    public String delete(@PathVariable Long id, HttpServletRequest request){
+        String token = (String) request.getSession().getAttribute("sessionToken");
+        String userRole = (String) request.getSession().getAttribute("sessionRole");
+
+        if (userRole == null || !userRole.equals("WORKER")) {
+            return "forward:/error";
+        }
+
+        workingHoursClient.delete(id,token); // Pass the token to the client
+        return "redirect:/working-hours";
+    }
+
+}
