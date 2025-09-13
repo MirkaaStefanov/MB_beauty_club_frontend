9860589bd4a25df3d34658c8aef90764ea5f1dd6
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-09-13 14:46:34 +0300

    add all users to user client

--- src/main/java/com/example/MB_beauty_club_frontend/clients/UserClient.java
+++ src/main/java/com/example/MB_beauty_club_frontend/clients/UserClient.java
@@ -2,13 +2,20 @@ package com.example.MB_beauty_club_frontend.clients;
 
 import com.example.MB_beauty_club_frontend.dtos.auth.PublicUserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
 
 @FeignClient(name = "MB-users", url = "${backend.base-url}/users")
 public interface UserClient {
 
     @GetMapping("/me")
     PublicUserDTO getMe(@RequestHeader("Authorization") String auth);
+
+    @GetMapping("/all")
+    List<PublicUserDTO> getAllUsers();
+
 
 }
