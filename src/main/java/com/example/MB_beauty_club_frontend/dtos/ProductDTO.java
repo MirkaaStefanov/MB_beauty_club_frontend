c1717cf724a968a0f276d40251ebdcd013536f3e
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-09-17 22:21:59 +0300

    promotion on allADMIN

--- src/main/java/com/example/MB_beauty_club_frontend/dtos/ProductDTO.java
+++ src/main/java/com/example/MB_beauty_club_frontend/dtos/ProductDTO.java
@@ -29,6 +29,7 @@ public class ProductDTO {
     private String image;
     @JsonIgnore
     private transient MultipartFile imageFile;
+    private boolean promotion;
     private int percent;
     private BigDecimal promotionPrice;
     private BigDecimal promotionEuroPrice;
