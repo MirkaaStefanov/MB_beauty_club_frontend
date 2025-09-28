a29b65a4c089c7e2251526da2300daf54964e85d
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-09-28 17:21:22 +0300

    create need products

---
+++ src/main/java/com/example/MB_beauty_club_frontend/dtos/NeedProductDTO.java
@@ -0,0 +1,19 @@
+package com.example.MB_beauty_club_frontend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NeedProductDTO {
+
+    private Long id;
+    private ProductDTO product;
+    private int quantity;
+
+
+}
